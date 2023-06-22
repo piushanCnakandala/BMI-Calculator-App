19af0b612b3c84948e1250a99dce5186e2be9015
piushanCnakandala <piushancnakandala2000@gmail.com>
2023-06-22 19:38:39 +0530

    Exteacting Widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,12 +9,110 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
+        appBar: AppBar(
+          title: Center(child: Text('BMI CALCULATOR')),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard(Color(0xFF1D1E33))),
+                Expanded(child: ReusableCard(Color(0xFF1D1E33))),
+              ],
+            )),
+            Expanded(child: ReusableCard(Color(0xFF1D1E33))),
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard(Color(0xFF1D1E33))),
+                Expanded(child: ReusableCard(Color(0xFF1D1E33))),
+              ],
+            )),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  ReusableCard(@required this.colour);
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color:colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
 }
+
+/*
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('BMI CALCULATOR')),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(15.0),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1D1E33),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        )),
+                    Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(15.0),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1D1E33),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        )),
+                  ],
+                )),
+            Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                )),
+            Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(15.0),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1D1E33),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        )),
+                    Expanded(
+                        child: Container(
+                          margin: EdgeInsets.all(15.0),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1D1E33),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        )),
+                  ],
+                )),
+          ],
+        ));
+  }
+}
+*/
